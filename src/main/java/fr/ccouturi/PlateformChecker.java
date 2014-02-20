655551f5e367f20476ddd41d2453d33367bf1130
Cyril Couturier <cyril.couturier@novapost.fr>
2014-02-20 15:01:13 +0100

    Add cache for plateform results.

--- src/main/java/fr/ccouturi/PlateformChecker.java
+++ src/main/java/fr/ccouturi/PlateformChecker.java
@@ -2,6 +2,9 @@ package fr.ccouturi;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -11,6 +14,8 @@ import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 
 public class PlateformChecker {
 
@@ -18,7 +23,16 @@ public class PlateformChecker {
 
     private static final Integer THREAD_COUNT = 5;
 
+    private static final long CACHE_EXPIRATION_PERIOD = 5;// SECONDS
+
+    protected static Cache<String, Object> cache = CacheBuilder.newBuilder()//
+            .expireAfterWrite(CACHE_EXPIRATION_PERIOD, TimeUnit.SECONDS) //
+            .build();
+
     // ---------------------------------------------------------------------------------------------
+
+    @JsonIgnore
+    private String key = UUID.randomUUID().toString();
 
     @JsonProperty
     private String name;
@@ -40,34 +54,49 @@ public class PlateformChecker {
     }
 
     @JsonProperty
-    public Result[] getResults() {
+    public Result[] getCacheResult() {
+        LOGGER.debug("Get cached result or check.");
         long start = System.currentTimeMillis();
-        ExecutorService execute = Executors.newFixedThreadPool(THREAD_COUNT);
-        executeRunnables(execute);
+        try {
+            Result[] results = (Result[]) cache.get(key, new Callable<Result[]>() {
+                @Override
+                public Result[] call() {
+                    return check();
+                }
+            });
+            LOGGER.info("Check duration (milliseconds): " + (System.currentTimeMillis() - start));
+            return results;
+        } catch (ExecutionException e) {
+            LOGGER.error(e.getMessage(), e);
+            return null;
+        }
+    }
 
+    @JsonIgnore
+    private Result[] check() {
+        final ExecutorService pool = Executors.newFixedThreadPool(THREAD_COUNT);
+        for (Runnable r : runnables) {
+            pool.execute(r);
+        }
+        pool.shutdown();
+        LOGGER.info("Waiting for healthchecks ending.");
+        try {
+            if (!pool.awaitTermination(60, TimeUnit.SECONDS)) {
+                LOGGER.error("Pool did not terminate");
+            }
+        } catch (InterruptedException e) {
+            pool.shutdownNow();
+        }
+        return collectResults();
+    }
+
+    @JsonIgnore
+    private Result[] collectResults() {
         Result[] results = new Result[checkers.length];
         for (int index = 0; index < checkers.length; ++index) {
             results[index] = checkers[index].result;
         }
-        LOGGER.info("Check duration (milliseconds): " + (System.currentTimeMillis() - start));
         return results;
-    }
-
-    @JsonIgnore
-    private void executeRunnables(final ExecutorService service) {
-        for (Runnable r : runnables) {
-            service.execute(r);
-        }
-        service.shutdown();
-        LOGGER.info("Waiting for healthchecks ending.");
-        try {
-            if (!service.awaitTermination(60, TimeUnit.SECONDS)) {
-                LOGGER.error("Pool did not terminate");
-            }
-        } catch (InterruptedException e) {
-            service.shutdownNow();
-        }
-        LOGGER.info("Done.");
     }
 
 }
